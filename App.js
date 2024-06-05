b61efa3881daa1084648efb5a9d2b3e47e6fd6a3
Ana Clara <acoferreia@sga.pucminas.br>
2024-06-05 14:32:08 -0300

    subindo projeto

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import Homepage from './src/pages/Homepage'
+
+const App = () => {
+ 
+   return (
+
+   <Homepage />
+
+   );
+}
+
+export default App;
