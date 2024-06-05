9c5035ec8e72fcd01e312ddf8c7cc0d2615d4a03
Ana Clara <acoferreia@sga.pucminas.br>
2024-06-05 14:14:45 -0300

    subindo projeto

---
+++ src/pages/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Homepage = () => {
+ 
+   return (
+
+   <View style={{flex:1, alignItems:'center', justifyContent:'center'}}> 
+   
+        <Text> Puc Minas </Text>
+   
+   </View>
+
+   );
+}
+
+export default Homepage;
